965069ca0d51ce627bd49b3cca3baf4043410678
datmanydocris <datmanydocris@hotmail.com>
2026-08-02 01:28:18 -0400

    A: API access, token printing and command line prompts implements

---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from openai import OpenAI
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+
+    if api_key is None:
+        raise RuntimeError("Error: OPENROUTER_API_KEY environment variable not set")
+
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    messages = [
+        {
+            "role": "user",
+            "content": args.user_prompt,
+        }
+    ]
+
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=messages,
+    )
+
+    # Check that the usgae data exists
+    if response.usage is None:
+        raise RuntimeError("API response did not include usage information")
+
+    # Print token counts
+    print(f"Prompt tokens: {response.usage.prompt_tokens}")
+    print(f"Response tokens: {response.usage.completion_tokens}")
+
+    print(response.choices[0].message.content)
+
+
+if __name__ == "__main__":
+    main()
